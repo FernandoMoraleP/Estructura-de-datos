914d89d41f642c76ebc1faed5201f368df477de1
Fernando Morales <derox59@hotmail.com>
2016-04-12 20:54:43 -0500

    Apuntadores

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: YANET
+ *
+ * Created on 11 de abril de 2016, 10:52
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int a;
+    int *a_ptr;
+    a_ptr = &a;
+    *a_ptr = 24;
+    cout << "El resultado es:" << a << "\n";
+
+    return 0;
+}
+
