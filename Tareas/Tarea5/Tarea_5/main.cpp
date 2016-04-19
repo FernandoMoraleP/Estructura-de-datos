178b71529015d3043ec85b29af598567510bf143
Fernando Morales <derox59@hotmail.com>
2016-04-19 16:49:09 -0500

    Tarea 5

---
+++ Tareas/Tarea5/Tarea_5/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: YANET
+ *
+ * Created on 19 de abril de 2016, 16:31
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Para el inciso 1 la respuesta se encuentra en el inciso 3\n";
+    cout << "Para el inciso 2 la respuesta se encuentra en el inciso 4\n";
+    cout << "Y viceversa.";
+    return 0;
+}
+
