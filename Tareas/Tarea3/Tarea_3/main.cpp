ae250397232d6ac95fe7d5d67b610e863de952b0
Fernando Morales <derox59@hotmail.com>
2016-04-18 20:03:35 -0500

    Tarea 3

---
+++ Tareas/Tarea3/Tarea_3/main.cpp
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: YANET
+ *
+ * Created on 17 de abril de 2016, 17:51
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Al primer programa le falta el alojamiento de memoria para el arreglo, int *arr = new int[ NUM ];";
+    /*int *arr = new int[ARRAY SIZE];*/
+    cout << "La validación para que no este vacío. \n";
+    /*
+     * if( arr == NULL ){
+    cout << "no se pudo alojar memoria suciente" << endl;
+    return -1;
+  };*/
+    cout << "El llenado del arreglo";
+    /*ifstream entrada;
+entrada.open("datos.txt");
+for( int i=0; i<ARRAY SIZE; i++ )
+entrada >> arr[i];*/
+    cout << "La lectura del mismo";
+    /*
+     * entrada.close();
+for( int i=0; i<ARRAY SIZE; i++ )
+cout << i << " " << arr[i];
+     */
+
+    cout << "\n Al segundo programa le falta:\n";
+    cout << "La validación para que no este vacío. \n ";
+    /*
+     * if( arr == NULL ){
+    cout << "no se pudo alojar memoria suciente" << endl;
+    return -1;
+  };*/
+    cout << "El llenado del arreglo\n";
+    /*ifstream entrada;
+entrada.open("datos.txt");
+for( int i=0; i<ARRAY SIZE; i++ )
+entrada >> arr[i];*/
+    cout << "La lectura del mismo";
+    /*entrada.close();
+for( int i=0; i<ARRAY SIZE; i++ )
+cout << i << " " << arr[i];*/
+
+    cout << "\n Al tercer programa le falta:\n";
+    cout << "La validación para que no este vacío. \n ";
+    /*
+     * if( arr == NULL ){
+    cout << "no se pudo alojar memoria suciente" << endl;
+    return -1;
+  };*/
+    cout << "El llenado del arreglo\n";
+        /*ifstream entrada;
+entrada.open("datos.txt");
+for( int i=0; i<ARRAY SIZE; i++ )
+entrada >> arr[i];*/
+    cout << "La lectura del mismo";
+       /*entrada.close();
+for( int i=0; i<ARRAY SIZE; i++ )
+cout << i << " " << arr[i];*/
+    return 0;
+}
+
